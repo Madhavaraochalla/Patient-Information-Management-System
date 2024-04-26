5af34d677449deccc3933e260137133382d15b1b
MADHAV <152585154+Madhavaraochalla@users.noreply.github.com>
2024-04-26 13:11:38 +0530

    Delete Docker-python-Intregration directory

--- Docker-python-Intregration/Delete-different-images.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-image = input ("Enter the image name to download")
-subprocess.call("docker rmi %s"%image,shell=True)
-
-
-
-# to run this programme
-# python Delete-different-images.py
